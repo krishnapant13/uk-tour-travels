ff7e9a1df1ca549b0ec902d91c6c3d7216292a17
krishnapant13 <krishna555.pant@gmail.com>
2024-12-03 11:32:03 +0530

    carousel correction

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -17,7 +17,6 @@ const Carousel: React.FC = () => {
     autoplay: true,
     autoplaySpeed: 2000,
     arrows: false,
-    centerPadding:"1%",
     responsive: [
       {
         breakpoint: 768,
@@ -35,32 +34,23 @@ const Carousel: React.FC = () => {
   };
 
   return (
-    <div
-      style={{
-        margin: "20px",
-        maxWidth: "100%",
-        marginLeft: "auto",
-        marginRight: "auto",
-      }}
-    >
+    <div className="w-full max-w-[1200px] mx-auto overflow-hidden">
       <Slider {...settings}>
         {citiesData.map((city, index) => (
-          <div key={index} className="relative group overflow-hidden">
+          <div key={index} className="relative px-2">
             <Image
               src={city.image}
               alt={`Slide ${index + 1}`}
               width={500}
               height={300}
-              className="object-cover w-[95%] h-[20em] transition-transform duration-300 group-hover:scale-105 rounded-lg"
+              className="object-cover rounded-lg w-full h-[20em]"
             />
-            <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-20 blur-sm transition duration-300 rounded-lg cursor-pointer"></div>
-
-            <p className="absolute top-4 left-4 px-2 py-1 rounded-full  bg-blue-700  text-[0.8em] text-white">
+            <p className="absolute top-4 left-4 px-2 py-1 rounded-full bg-blue-700 text-[0.8em] text-white">
               {city.attractions.length} Tours
             </p>
-            <p className="absolute bottom-4 left-4 px-2 py-1  text-md text-white flex flex-col justify-start items-start">
+            <p className="absolute bottom-4 left-4 px-2 py-1 text-md text-white flex flex-col justify-start items-start">
               From
-              <span className=" font-bold">{city.city}</span>
+              <span className="font-bold">{city.city}</span>
             </p>
           </div>
         ))}
