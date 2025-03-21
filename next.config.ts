b649407f3bc3fc5898f19064e0951cde791e3673
krishnapant13 <krishna555.pant@gmail.com>
2025-03-21 12:05:33 +0530

    restructure file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["i.pinimg.com"],
   },
+  async rewrites() {
+    return [
+      { source: "/robots.txt", destination: "/robots" },
+      { source: "/sitemap.xml", destination: "/sitemap" },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
