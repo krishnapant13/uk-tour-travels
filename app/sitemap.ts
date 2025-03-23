909919df2ff3c92c12bd50013bb3d9119778f872
krishnapant13 <krishna555.pant@gmail.com>
2025-03-23 05:55:52 +0530

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,23 +3,34 @@ import citiesData from "@/app/citiesData.json";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://uttarakhandtravelss.com";
+  const lastModified = new Date().toISOString();
 
-  // Static Pages
   const routes: MetadataRoute.Sitemap = [
-    { url: `${baseUrl}/`, lastModified: new Date().toISOString() },
-    { url: `${baseUrl}/about`, lastModified: new Date().toISOString() },
+    { url: `${baseUrl}/`, lastModified },
+    { url: `${baseUrl}/about`, lastModified },
+    { url: `${baseUrl}/contact`, lastModified },
+    { url: `${baseUrl}/booking`, lastModified },
   ];
 
-  // Dynamically Add City and Attraction Pages
+  // Function to format attraction URLs
+  const formatUrl = (
+    city: string,
+    attraction: { name: string; header: string }
+  ) => {
+    const formattedAttraction = `${attraction.name
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")}-${attraction.header
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")}`;
+    return `${baseUrl}/${city.toLowerCase()}/${formattedAttraction}`;
+  };
+
+  // Add city and attraction routes
   citiesData.forEach((city) => {
     city.attractions.forEach((attraction) => {
-      const formattedAttraction = `${attraction.name
-        .toLowerCase()
-        .replace(/[^a-z0-9]+/g, "-")}`;
-
       routes.push({
-        url: `${baseUrl}/${city.city.toLowerCase()}/${formattedAttraction}`,
-        lastModified: new Date().toISOString(),
+        url: formatUrl(city.city, attraction),
+        lastModified,
       });
     });
   });
